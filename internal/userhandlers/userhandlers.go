adc0ebe9a3de1be800056415fbfe93e1475377ca
Valchuk Elena <elena.valchuk@gmail.com>
2024-02-16 13:52:24 +0300

    added allow headers for origin function

--- internal/userhandlers/userhandlers.go
+++ internal/userhandlers/userhandlers.go
@@ -121,13 +121,11 @@ func Register(rw http.ResponseWriter, r *http.Request) {
 
 func Login(rw http.ResponseWriter, r *http.Request) {
 
-	if origin := r.Header.Get("Origin"); origin != "" {
-        rw.Header().Set("Access-Control-Allow-Origin", origin)
-        rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-        rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		rw.Header().Set("Access-Control-Expose-Headers", "Authorization")
-		rw.Header().Set("Access-Control-Allow-Credentials", "true")
-    }
+    rw.Header().Set("Access-Control-Allow-Origin", "*")
+    rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	rw.Header().Set("Access-Control-Expose-Headers", "Authorization")
+	rw.Header().Set("Access-Control-Allow-Credentials", "true")
 
 
 	if r.Method == "OPTIONS" {
