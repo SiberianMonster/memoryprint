e7159d53597b8f6b342ab27e14df981dcbe83c3f
Valchuk Elena <elena.valchuk@gmail.com>
2024-02-20 16:22:18 +0300

    added debug print to the middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -32,12 +32,14 @@ func MiddlewareCORSHeaders(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		log.Printf("Setting headers:")
 	
 	
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+		w.Header().Set("Content-Type", "application/json")
 	
         next.ServeHTTP(w, r)
     })
