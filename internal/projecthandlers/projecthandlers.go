212b88ad0f8582cf30abeb4fa38e3250484e1df0
Valchuk Elena <elena.valchuk@gmail.com>
2025-02-11 11:30:40 +0300

    added test photobook with all images

--- internal/projecthandlers/projecthandlers.go
+++ internal/projecthandlers/projecthandlers.go
@@ -2589,8 +2589,8 @@ func GenerateCreatingImageLinks(ctx context.Context, storeDB *pgxpool.Pool) {
 	}
 
 	for range ticker.C {
-		//var projectIDs []uint
-		var projectIDs []int{452}
+		var projectIDs []uint
+		projectIDs = append(projectIDs,452)
 		// projectIDs, err = projectstorage.LoadPublishedProjects(ctx, storeDB)
 		//if err != nil {
 		//	log.Printf("Error happened when retrieving published projects. Err: %s", err)
